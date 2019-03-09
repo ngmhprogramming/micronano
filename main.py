5821f1c1c22c406b249594f5efd8d5cf5a8b86a1
ngmh <ngmhcartoon@gmail.com>
2019-03-09 14:45:25 +0800

    movement... not really

--- main.py
+++ main.py
@@ -20,18 +20,20 @@ with terminal.fullscreen():
     while True:
         for i in range(len(text)):
             with terminal.location(0, i):
-                if cursor[0] is i:
+                if cursor[0] is i  and len(text[i]) > 0:
                     print(text[i][:cursor[1]]+
                           terminal.black+terminal.on_white+text[i][cursor[1]]+
                           terminal.white+terminal.on_black+text[i][cursor[1]+1:])
                 else: print(text[i])
-
-        print(len(text))
         char = curse.getkey()
         if char is "q":
             endwin()
             exit()
-        elif char == "KEY_UP" and cursor[0] > 0: cursor[0] -= 1
-        elif char == "KEY_DOWN" and cursor[0] < len(text)-1: cursor[0] += 1
+        elif char == "KEY_UP" and cursor[0] > 0:
+            cursor[0] -= 1
+            cursor[1] = min(cursor[1], len(text[cursor[0]])-1)
+        elif char == "KEY_DOWN" and cursor[0] < len(text)-1:
+            cursor[0] += 1
+            cursor[1] = min(cursor[1], len(text[cursor[0]])-1)
         elif char == "KEY_LEFT" and cursor[1] > 0: cursor[1] -= 1
         elif char == "KEY_RIGHT" and cursor[1] < len(text[cursor[0]])-1: cursor[1] += 1
